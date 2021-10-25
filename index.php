adde0ad78d25ce79be02fb3094a09c1035458273
muhammad-yazdian <muhammad.yazdian@gmail.com>
2021-10-25 15:17:56 +0200

    Project started. Outline included.

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+/* 
+ * File:    index.php
+ * 
+ * Author:  S. Muhammad Yazdian (muhammad.yazdian@gmail.com) 
+ * Date:    2021-10-25 
+ * 
+ * Summary of File: 
+ * 
+ *   This file contains code which creates a HTML HMI, communicates to a server
+ *   and displays information for countries. 
+ *
+ * Development Steps:
+ *   1- Fetch list of countires form database
+ *   2- Create a country select menue
+ *   3- Send a request to the server
+ *   4- Display received information from the server
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Server Client Project</title>
+</head>
+<body>
+    
+</body>
+</html>
